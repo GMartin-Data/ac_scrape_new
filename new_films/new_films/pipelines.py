91c89feb00cec59a32bd00196fcc0c63129fa765
GMartin-Data <gregory.martin.data@gmail.com>
2024-04-05 17:58:13 +0200

    Add CleanPipeline

--- new_films/new_films/pipelines.py
+++ new_films/new_films/pipelines.py
@@ -5,9 +5,52 @@
 
 
 # useful for handling different item types with a single interface
+from typing import Optional
+
 from itemadapter import ItemAdapter
+from loguru import logger
 
 
-class NewFilmsPipeline:
+LIST_FIELDS = ("casting", "director", "genres")
+SET_FIELDS = ("societies", )
+
+
+def convert_duration(duration: str) -> Optional[int]:
+    """Convert a duration string to its minutes counterpart"""
+    if duration is None or not duration.endswith("min"):
+        return None
+    duration = duration.split()
+    hours = int(duration[0].replace("h", ""))
+    minutes = int(duration[1].replace("min", ""))
+    return (60 * hours + minutes)
+
+
+class CleanPipeline:
+    @logger.catch
     def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+
+        for field in LIST_FIELDS:
+            value = adapter.get(field)
+            try:
+                value = [item.strip() for item in value]
+                adapter[field] = "|".join(value)
+            except BaseException:
+                adapter[field] = None
+
+        for field in SET_FIELDS:
+            value = adapter.get(field)
+            try:
+                value = set(item.strip() for item in value)
+                adapter[field] = "|".join(value)
+            except BaseException:
+                adapter[field] = None
+
+        # Special case of duration
+        duration = adapter.get("duration")
+        if duration is not None:
+            adapter["duration"] = convert_duration(duration)
+        else:
+            adapter["duration"] = None
+   
         return item
